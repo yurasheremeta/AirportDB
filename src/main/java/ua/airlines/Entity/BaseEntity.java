a09b0eb90039cbf505c44b2f6e26f2caa5b2bb4c
yurasheremeta <sheremeta.yura@gmail.com>
2018-07-21 11:17:13 +0300

    update baseEntity

--- src/main/java/ua/airlines/Entity/BaseEntity.java
+++ src/main/java/ua/airlines/Entity/BaseEntity.java
@@ -1,5 +1,16 @@
 package ua.airlines.Entity;
 
-public class BaseEntity {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 }
