4e7f62312ea3776738923b1293dde6215fce81ba
yurasheremeta <sheremeta.yura@gmail.com>
2018-07-21 11:12:52 +0300

    update aircompany

--- src/main/java/ua/airlines/Entity/Aircompany.java
+++ src/main/java/ua/airlines/Entity/Aircompany.java
@@ -1,5 +1,24 @@
 package ua.airlines.Entity;
 
-public class Aircompany {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name ="air_company")
+public class Aircompany extends BaseEntity {
+	
+		@Column(name = "company_name" , length = 150 , nullable = false)
+		private String companyName;
+		
+		@Column(name = "company_addres" , nullable = false)
+		private String companyAddres;
 }
